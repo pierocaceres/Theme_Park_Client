715f866fb254661662267b539710bc714fe2c8f3
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-24 12:11:34 -0400

    Navigation/Home implemented in App.js

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     
-    
     return (
           <nav className="nav" >
             <h1> Disney Parks </h1>
             <div>
-                <Link to="/parks"> Parks </Link>
                 <Link to="/"> Home </Link>
+                <Link to="/parks"> Parks </Link>
                 <Link to="/coasters"> Coasters </Link>
-
             </div>    
         </nav>
     );

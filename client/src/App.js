c0f4d4c9df9f44762ba93ca3f7c683470015a7e7
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-24 12:02:48 -0400

    Created useState for Parks and Roller Coaster

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div className="App">
       <header>
-
+        <Navigation />
       </header>
       <main>
         <Routes>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
+import { useNavigation } from 'react-router-dom'
+import axios from 'axios'
+import ThemePark from './ThemePark'
 
 function Home(props) {
+    const [themeParks, setThemeParks] = useState([])
+    const [rollerCoaster, setRollerCoaster] = useState([])
+    
+    let navigate = useNavigation()
+
+    const getThemePark = async () => {
+        const result = await axios.get() 
+        setThemeParks(result)   
+    }
+
+    const getRollerCoaster = async () => {
+        const result = await axios.get()
+        setRollerCoaster(result)//Look at result path
+    }
+
+    useEffect( () => {
+        getThemePark()
+        getRollerCoaster()
+    }, [])
+
     return (
         <div>
-            
+            <div className='Theme-Park'>
+                <h2>Theme Parks</h2>
+                {/* <ThemePark key={themeParks.id}
+                name={themeParks.name}
+                description={themeParks.description}
+                location={themeParks.location}
+                image={themeParks.image}
+                /> */}
+            </div>
+            <div className='Roller-Coaster'>
+                <h2>Roller Coaster</h2>
+
+            </div>
         </div>
     );
 }
